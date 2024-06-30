ef39f9be836a475125bbb1fabd261d6253279332
priyanka <priyanka.work12@gmail.com>
2024-06-30 09:59:07 +0200

    chore: Revsion of ts basics and terminal commands

---
+++ revision.ts
@@ -0,0 +1,16 @@
+interface Languages{      //interface alwys {}. First define , then create 
+    germany: string;
+    spain: string;
+    india: string;
+
+};
+
+const myLanguages: Languages[]=[
+    {germany: 'german', spain: 'spanish', india: 'hindi'},  //array separated by commas
+    
+
+];
+
+let name4 = 'Priyanka';
+
+console.log('hi');
